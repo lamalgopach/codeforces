421c3a2c2024a4e7a3425e3c4536f77ba575a35d
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2021-03-09 16:47:30 -0800

    Cleaned code.

--- Hackerrank/roads_and_libraries.py
+++ Hackerrank/roads_and_libraries.py
@@ -1,3 +1,37 @@
+# Challenge from Hackerrank
+
+# Determine the minimum cost to provide library access to all citizens of HackerLand. 
+# There are n cities numbered from 1 to n. 
+# Currently there are no libraries and the cities are not connected. 
+# Bidirectional roads may be built between any city pair listed in cities. 
+
+# A citizen has access to a library if:
+# -Their city contains a library.
+# -They can travel by road from their city to a city containing a library.
+
+# Input:
+
+# The first line contains a single integer q, that denotes the number of queries.
+# The subsequent lines describe each query in the following format:
+# - The first line contains four space-separated integers that describe the respective 
+# values of n, m, c_lib, c_road - 
+# the number of cities, number of roads, cost of a library and cost of a road.
+
+# - Each of the next m lines contains two space-separated integers, u[i] and v[i], 
+# that describe a bidirectional road that can be built to connect cities u[i] and v[i].
+
+
+# Output:
+# int: the minimal cost
+
+
+def modify_dict(old_set, new_set, dict_):
+	for i in old_set:
+		dict_[i] = new_set
+	return dict_
+
+
+
 def roadsAndLibraries(n, c_lib, c_road, cities):
 	if c_lib <= c_road:
 		return c_lib * n
@@ -15,32 +49,30 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
 				break
 
 			if neighbours[0] in cities_set and neighbours[1] in cities_set:
-				first_set, second_set = None, None
 				first_set = cities_dict.get(neighbours[0])
 				second_set = cities_dict.get(neighbours[1])
+
 				if first_set != second_set:
 					union_set = first_set.union(second_set) 
-					for c_i in first_set:
-						cities_dict[c_i] = union_set
-					for c_i in second_set:
-						cities_dict[c_i] = union_set
+
+					cities_dict = modify_dict(first_set, union_set, cities_dict)
+					cities_dict = modify_dict(second_set, union_set, cities_dict)
 
 					road_num += 1
 					lib_num -= 1
 
 			elif neighbours[0] in cities_set or neighbours[1] in cities_set:
+				old_neighbour = neighbours[0] if neighbours[0] in cities_set else neighbours[1]
+				new_neighbour = neighbours[0] if neighbours[0] not in cities_set else neighbours[1]
+				cities_set.add(new_neighbour)
+				cities_dict[old_neighbour].add(new_neighbour)
+
+				old_set = cities_dict.get(old_neighbour)
+				new_set = old_set 
+
+				cities_dict = modify_dict(old_set, new_set, cities_dict)
 
 				road_num += 1
-
-				present_neighbour = neighbours[0] if neighbours[0] in cities_set else neighbours[1]
-				unpresent_neighbour = neighbours[0] if neighbours[0] not in cities_set else neighbours[1]
-				cities_set.add(unpresent_neighbour)
-
-
-				cities_dict[present_neighbour].add(unpresent_neighbour)
-				set_present = cities_dict.get(present_neighbour)
-				for c_i in set_present:
-					cities_dict[c_i] = set_present
 
 			elif neighbours[0] not in cities_set and neighbours[1] not in cities_set:
 
